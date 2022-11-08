8932602b37ba36f7ea259798d2b8e4aadc511cd5
panwen123456 <a@b.com>
2022-11-08 17:12:52 +0800

    init vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/components/Login'
+import Notebooks from '@/components/noteBookList'
+import NoteDetail from '@/components/NoteDetail'
+import TrashDetail from '@/components/TrashDetail'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +13,23 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: Notebooks
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
     }
   ]
 })
