d4fe95a61928a288cd9e123d69046f31a700b6b1
panwen123456 <a@b.com>
2022-11-09 20:26:32 +0800

    Login

---
+++ src/components/NoteDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 笔记本详情页 -->
+  <div id="note-detail">
+    <h1>{{msg}} : {{$route.params.noteId}}</h1>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'Login',
+  data() {
+    return {
+      msg: '笔记本详情页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/components/TrashDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 回收站笔记本详情页 -->
+  <div id="trash-detail">
+    <h1>{{msg}} : {{$route.params.noteId}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        msg: '回收站笔记本详情页'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
